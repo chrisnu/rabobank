acfca7d0eafd5a85585ebacceff27e2cb1a22fa8
chris <chrisnu@outlook.com>
2020-03-02 14:20:21 +0100

    Set enum card status

--- src/main/java/com/rabobank/chris/model/Card.java
+++ src/main/java/com/rabobank/chris/model/Card.java
@@ -1,10 +1,12 @@
 package com.rabobank.chris.model;
 
+import com.rabobank.chris.model.enums.CardStatus;
 import lombok.Data;
 
 @Data
 public abstract class Card {
     String id;
+    CardStatus cardStatus;
     Integer cardNumber;
     Integer squenceNumber;
     String cardHolder;
