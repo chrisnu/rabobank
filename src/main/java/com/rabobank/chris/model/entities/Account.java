ef3976bc0905e77adb05617db1a16d8db8953415
chris <chrisnu@outlook.com>
2020-04-01 00:21:49 +0200

    Entity account unit test

--- src/main/java/com/rabobank/chris/model/entities/Account.java
+++ src/main/java/com/rabobank/chris/model/entities/Account.java
@@ -1,6 +1,7 @@
 package com.rabobank.chris.model.entities;
 
 import lombok.Getter;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -10,9 +11,28 @@ import java.util.Calendar;
 @Table(name = "account")
 public class Account {
 
+    protected Account() {
+    }
+
+    public Account(Integer id, String owner, float balance, Calendar created, Calendar ended) {
+        Assert.hasLength(owner, "Account name should not be empty");
+        Assert.isTrue(id > 0, "ID should be greater than 0");
+        Assert.notNull(created, "Created should not be null");
+        if (ended != null) {
+            Assert.isTrue(ended.after(created), "Ended should be after created date");
+        }
+
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+        this.created = created;
+        this.ended = ended;
+    }
+
     /**
      * In order to use database auto incremental flag, we set the generation type to IDENTITY
      */
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -22,15 +42,18 @@ public class Account {
     @NotNull
     private String owner;
 
+    @Getter
     @Column(length = 13, precision = 3, nullable = false)
     @NotNull
     private Float balance;
 
+    @Getter
     @Column(nullable = false)
     @Temporal(TemporalType.DATE)
     @NotNull
     private Calendar created;
 
+    @Getter
     @Temporal(TemporalType.DATE)
     private Calendar ended;
 }
---
+++ src/test/java/com/rabobank/chris/model/entities/AccountTest.java
@@ -0,0 +1,36 @@
+package com.rabobank.chris.model.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+
+class AccountTest {
+
+    private static final Integer EXPECTED_ACCOUNT_ID = 1;
+    private static final String EXPECTED_OWNER = "Owner";
+    private static final float EXPECTED_BALANCE = 50;
+    private static final float EXPECTED_CREATED_YEAR = 2020;
+    private static final float EXPECTED_CREATED_MONTH = 0;
+    private static final float EXPECTED_CREATED_DAY = 1;
+
+    Account account;
+
+    @BeforeEach
+    public void setUp() {
+        Calendar created = Calendar.getInstance();
+        created.set(2020, Calendar.JANUARY , 1);
+        account = new Account(1, "Owner", 50, created, null);
+    }
+
+    @Test
+    public void testAccount() {
+        Assertions.assertEquals(EXPECTED_ACCOUNT_ID, account.getId());
+        Assertions.assertEquals(EXPECTED_OWNER, account.getOwner());
+        Assertions.assertEquals(EXPECTED_BALANCE, account.getBalance());
+        Assertions.assertEquals(EXPECTED_CREATED_YEAR, account.getCreated().get(Calendar.YEAR));
+        Assertions.assertEquals(EXPECTED_CREATED_MONTH, account.getCreated().get(Calendar.MONTH));
+        Assertions.assertEquals(EXPECTED_CREATED_DAY, account.getCreated().get(Calendar.DAY_OF_MONTH));
+    }
+}
